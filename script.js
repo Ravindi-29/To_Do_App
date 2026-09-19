5dd7f2d9e412f00757a51ae73c29b1e3f9a7eb18
Ravindi-29 <rranthilini29@gmail.com>
2026-09-19 19:12:08 +0530

    Update API_URL to correct Azure backend domain

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Constants
-const API_URL = "https://to-do-backend-app.azurewebsites.net";
+const API_URL = "https://to-do-backend-app-c5a4ecfbcageghfw.eastasia-01.azurewebsites.net";
 const DAYS_OF_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 // State
